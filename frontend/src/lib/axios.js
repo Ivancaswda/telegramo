e2b4031b9a1f54ccef7b5a4a16f11a8c14648408
ScienceProject.io <162120681+Ivancaswda@users.noreply.github.com>
2025-03-13 10:03:29 +0200

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:  'http://localhost:1120/api',
+    baseURL:  'https://telegrambackend-sand/api',
     withCredentials: true
 })
